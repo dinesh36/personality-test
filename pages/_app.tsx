9977ca22c4805108ec1944c5010986db162abca9
tejas <dinesh.dabhi@rapidops.com>
2022-05-10 23:22:24 +0530

    Set the layout for the home page and ads.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import AppLayout from '../src/modules/layout/app-layout/app-layout';
 import type { AppProps } from 'next/app'
+import 'antd/dist/antd.css';
 
 function PersonalityTest({ Component, pageProps }: AppProps) {
   return <AppLayout>
